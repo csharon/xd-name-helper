6775965be9c74484b1c51ccb7b769a32dd3df531
Megan Sharon <me@megansharon.com>
2014-11-10 14:04:22 -0500

    add support for hyphenating module names

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+/*jshint node:true, es3:false*/
+/*globals require, it, describe*/
 var _ = require('lodash'),
-    mout = require('mout');
+  mout = require('mout');
 
 exports.classify = function (name) {
-    return mout.string.pascalCase(name);
+  return mout.string.pascalCase(name);
 };
 
 exports.titleize = function (name) {
-    return mout.string.properCase(mout.string.replace(mout.string.hyphenate(name), ['-', '_'], ' '));
+  return mout.string.properCase(mout.string.replace(mout.string.hyphenate(name), ['-', '_'], ' '));
 };
 
 exports.hyphenate = function (name) {
-    return mout.string.replace(mout.string.hyphenate(name), ['_'], '-')
+  var name = name.split('.').join('-');
+  return mout.string.replace(mout.string.hyphenate(name), ['_'], '-')
 }
 
 exports.camelize = function (name) {
-    return mout.string.camelCase(name)
+  return mout.string.camelCase(name)
 }
